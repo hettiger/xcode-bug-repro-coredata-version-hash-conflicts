334ac9ade140b18348f4cb18bff95abf9a98423f
Martin Hettiger <martin@hettiger.com>
2021-02-05 20:08:31 +0100

    Setup test case, model and mapping model to reproduce bug

--- CoreDataVersionHashConflictsTests/CoreDataVersionHashConflictsTests.swift
+++ CoreDataVersionHashConflictsTests/CoreDataVersionHashConflictsTests.swift
@@ -5,29 +5,74 @@
 //  Created by Martin Hettiger on 05.02.21.
 //
 
-import XCTest
+import CoreData
 @testable import CoreDataVersionHashConflicts
+import XCTest
 
+/// CoreData Version Hash Conflicts Tests
+///
+/// This test case reproduces an issue with the CoreData attribute »Preserve After Deletion« option.
+/// Follow these simple steps to make all tests pass:
+///
+/// 1. Disable Preserve After Deletion on `Entity.id` in both model versions
+/// 2. Open the mapping model; execute »Editor / Refresh Data Models«.
 class CoreDataVersionHashConflictsTests: XCTestCase {
+    var model: NSManagedObjectModel!
+    var model2: NSManagedObjectModel!
+    var mapping: NSMappingModel!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+
+        let modelPath = Bundle.main.path(
+            forResource: "CoreDataVersionHashConflicts",
+            ofType: "mom",
+            inDirectory: "CoreDataVersionHashConflicts.momd"
+        )!
+        let modelURL = URL(fileURLWithPath: modelPath)
+        model = NSManagedObjectModel(contentsOf: modelURL)!
+
+        let model2Path = Bundle.main.path(
+            forResource: "CoreDataVersionHashConflicts 2",
+            ofType: "mom",
+            inDirectory: "CoreDataVersionHashConflicts.momd"
+        )!
+        let model2URL = URL(fileURLWithPath: model2Path)
+        model2 = NSManagedObjectModel(contentsOf: model2URL)!
+
+        let mappingPath = Bundle.main.path(
+            forResource: "CoreDataVersionHashConflicts to CoreDataVersionHashConflicts 2",
+            ofType: "cdm"
+        )!
+        let mappingURL = URL(fileURLWithPath: mappingPath)
+        mapping = NSMappingModel(contentsOf: mappingURL)!
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        model = nil
+        model2 = nil
+        mapping = nil
+
+        super.tearDown()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    func test_model_and_mapping_version_hashes_are_matching() {
+        for entityMapping in mapping.entityMappings {
+            let modelSourceEntityVersionHash = model
+                .entityVersionHashesByName[entityMapping.sourceEntityName!]!
+            let mappingSourceEntityVersionHash = entityMapping.sourceEntityVersionHash!
+            XCTAssertEqual(modelSourceEntityVersionHash, mappingSourceEntityVersionHash)
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+            let modelDestinationEntityVersionHash = model2
+                .entityVersionHashesByName[entityMapping.destinationEntityName!]!
+            let mappingDestinationEntityVersionHash = entityMapping.destinationEntityVersionHash!
+            XCTAssertEqual(modelDestinationEntityVersionHash, mappingDestinationEntityVersionHash)
         }
     }
 
+    func test_finds_compatible_mapping_model() {
+        XCTAssertNotNil(
+            NSMappingModel(from: [.main], forSourceModel: model, destinationModel: model2)
+        )
+    }
 }
